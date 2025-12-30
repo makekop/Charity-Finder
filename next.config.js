3ed3ac456e464be996a6a2ecc5496c334306a358
makekop <72009584+makekop@users.noreply.github.com>
2025-12-30 19:49:27 +0200

    Add first version off Card

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            new URL(
+                "https://mieli.fi/wp-content/uploads/2021/06/MIELI-logo-1-2460x790.png"
+            ),
+        ],
+    },
+};

2ff7a3029af339e9a2e22f9d64035e744a903aee
makekop <72009584+makekop@users.noreply.github.com>
2026-01-12 17:00:57 +0200

    Remove the header titles from footer

--- components/ui/footer.jsx
+++ components/ui/footer.jsx
@@ -1,10 +1,6 @@
 export default function Footer() {
     const titles = ["KOTI", "MEISTÄ", "FAQ"];
     return (
-        <div className="flex flex-row justify-center gap-12 text-3xl m-8 font-bold">
-            {titles.map((title) => (
-                <p key={title}>{title}</p>
-            ))}
-        </div>
+        <div className="flex flex-row justify-center gap-12 text-3xl m-8 font-bold"></div>
     );
 }
